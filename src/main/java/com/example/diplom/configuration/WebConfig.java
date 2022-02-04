6cd68a8baf944b716d8b1e528f900e8aba5aae12
Artem Mirvoda <mirvodartem@gmail.com>
2022-02-04 21:40:28 +0500

    Переезд фронтенда на Vue3 и Vite. Конфигурация CORS.

--- src/main/java/com/example/diplom/configuration/WebConfig.java
+++ src/main/java/com/example/diplom/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.diplom.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
     }
 }
