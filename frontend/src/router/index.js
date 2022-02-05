30644934936b319f659c91f24b282dffc8d5496d
Artem Mirvoda <mirvodartem@gmail.com>
2022-02-05 21:11:02 +0500

    Не застейджил файлы

---
+++ frontend/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '/src/App.vue'
+const routes = [
+    // {
+    //     path: '/',
+    //     name: 'App',
+    //     component: App,
+    // },
+]
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+export default router
