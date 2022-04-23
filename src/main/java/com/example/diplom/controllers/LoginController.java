35671d00d9ad2aa57428b836eb051f6997d1e6ef
ApAcheGod <31448839+ApAcheGod@users.noreply.github.com>
2022-04-23 13:34:42 +0500

    fix

--- src/main/java/com/example/diplom/controllers/LoginController.java
+++ src/main/java/com/example/diplom/controllers/LoginController.java
@@ -1,19 +1,18 @@
 package com.example.diplom.controllers;
 
 import com.example.diplom.entities.User;
-import com.example.diplom.entities.dto.LoginDto;
 import com.example.diplom.services.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
@@ -23,37 +22,21 @@ import java.security.Principal;
 public class LoginController {
 
     private final UserServiceImpl userService;
-    private final AuthenticationManager authenticationManager;
-
-    @PostMapping(value = "/api/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity login(@RequestBody LoginDto loginDto){
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginDto.login(), loginDto.password()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        System.out.println(SecurityContextHolder.getContext());
-//        System.out.println(SecurityContextHolder.getContext());
-        System.out.println(SecurityContextHolder.getContextHolderStrategy());
-        return new ResponseEntity<>(authentication.getPrincipal(), HttpStatus.OK);
-    }
 
     @GetMapping(value = "/api/check", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity getInfo(){
-        System.out.println(SecurityContextHolder.getContext());
-        System.out.println(SecurityContextHolder.getContextHolderStrategy());
-        return new ResponseEntity(SecurityContextHolder.getContext().getAuthentication().getPrincipal() ,HttpStatus.OK);
+        return new ResponseEntity(SecurityContextHolder.getContext(), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/api/logout", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity logoutUser(){
-        SecurityContextHolder.clearContext();
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/resetPassword")
+    public String changePassword(Principal principal, Model model){
+        model.addAttribute("login", principal.getName());
+        return "resetPasswordPage";
     }
 
 
-    @PostMapping("/api/resetPassword")
+    @PostMapping("/resetPassword")
     @ResponseBody
     public ResponseEntity changePassword(HttpServletRequest request, Principal principal){
         String password = request.getParameter("password");
@@ -62,6 +45,13 @@ public class LoginController {
         userService.save(user);
         return new ResponseEntity(user, HttpStatus.OK);
     }
+
+//    @PostMapping("/login")
+//    public String loginUser(@RequestParam(name = "error", required = false) String error,
+//                            @RequestParam(name = "logout", required = false) String logout,
+//                            Model model) {
+//        return "redirect:main";
+//    }
 
 
 }
